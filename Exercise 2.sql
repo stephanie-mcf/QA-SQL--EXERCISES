8000c125d32db74b3ec73f222d11deb76f2f9d82
stephanie-mcf <116071524+stephanie-mcf@users.noreply.github.com>
2022-11-29 16:51:06 +0000

    Add files via upload

--- Exercise 2.sql
+++ Exercise 2.sql
@@ -1,1 +1,29 @@
-SELECT d.DelegateID, Sum(DurationDays) AS DelegateDays, Count(*)
+CREATE FUNCTION udf_DelegateDayss()
+RETURNS TABLE
+AS
+RETURN (
+	SELECT d.DelegateID, Sum(cr.DurationDays) AS DelegateDays, Count(*) AS DelegateCourses 
+		FROM Delegate as d
+	INNER JOIN DelegateAttendance as da
+	ON d.DelegateID = da.DelegateID
+	INNER JOIN CourseRun as cr
+	ON cr.CourseRunID = da.CourseRunID
+	GROUP BY d.DelegateID
+ )
+ GO
+ SELECT * FROM dbo.udf_DelegateDayss()
+
+CREATE FUNCTION udf_IndividualDelegateDayss (@DelegateID INT)
+RETURNS TABLE
+AS
+RETURN (
+	SELECT d.DelegateID, Sum(cr.DurationDays) AS DelegateDays, Count(*) AS DelegateCourses 
+		FROM Delegate as d
+	INNER JOIN DelegateAttendance as da
+	ON d.DelegateID = da.DelegateID
+	INNER JOIN CourseRun as cr
+	ON cr.CourseRunID = da.CourseRunID
+	GROUP BY d.DelegateID
+	)
+GO
+SELECT * FROM dbo.udf_IndividualDelegateDays(1)
